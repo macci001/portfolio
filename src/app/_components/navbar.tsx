f6e6cc6ae1a1d1a3a1ceeb44ca680f75e4347c7f
Maharshi Alpesh <maharshialpesh@Maharshi-Book.local>
2024-09-30 13:46:48 +0530

    some refactor and removing the contact me

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -12,48 +12,16 @@ export const NavbarComponent = () => {
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="#skills">
-            Skills
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
           <Link color="foreground" href="#project">
             Projects
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#contact-me">
-            Contact Me
+          <Link href="#skills">
+            Skills
           </Link>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
   )
-  // return (<div className="p-4 flex justify-end items-center gap-x-2 text-[1vw]">
-  //   <Link href={"#home"} className="p-2 rounded-md relative group">
-  //     <div className="relative text-lg md:text-2xl xl:text-3xl font-bold tracking-tight">
-  //       Home
-  //     <div className="absolute group-hover:bg-yellow-400 w-full h-4 -bottom-2 opacity-40"></div>
-  //     </div>
-  //   </Link>
-  //   <Link href={"#skills"} className="p-2 rounded-md relative group">    
-  //     <div className="relative text-xl md:text-2xl xl:text-3xl font-bold tracking-tight">
-  //       Skills
-  //     <div className="absolute group-hover:bg-yellow-400 w-full h-4 -bottom-2 opacity-40"></div>
-  //     </div>
-  //   </Link>
-  //   <Link href={"#project"} className="p-2 rounded-md relative group">
-  //     <div className="relative text-xl md:text-2xl xl:text-3xl font-bold tracking-tight">
-  //       Projects
-  //     <div className="absolute group-hover:bg-yellow-400 w-full h-4 -bottom-2 opacity-40"></div>
-  //     </div>
-  //   </Link>
-  //   <Link href={"#contact-me"} className="p-2 rounded-md relative group">
-  //     <div className="relative text-xl md:text-2xl xl:text-3xl font-bold tracking-tight">
-  //       Contact
-  //     <div className="absolute group-hover:bg-yellow-400 w-full h-4 -bottom-2 opacity-40"></div>
-  //     </div>
-  //   </Link>
-  // </div>
-  // )
 }
