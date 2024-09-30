a8656a3643e8c2e787915ff6d7365a6ca042ffde
Maharshi Alpesh <maharshialpesh@Maharshi-Book.local>
2024-09-30 14:20:49 +0530

    updating the project list

--- src/app/productList.ts
+++ src/app/productList.ts
@@ -1,8 +1,6 @@
-import anonymity from "./_components/images/docker.svg";
-
 const projectList = [
   {
-    name: "Secret-Feedback",
+    name: "Secret Feedback",
     summary: "An secret feedback sharing web application.",
     desc1: "AuthJS is used for the authentication",
     desc2: "Resend for user verification.",
@@ -19,10 +17,10 @@ const projectList = [
     ],
     projectUrl: "https://anonymity.maharshialpesh.com/",
     sourceCodeUrl: "https://github.com/macci001/anonymity",
-    org: "secret-feedback.com"
+    org: "secretfeedback.com"
   },
   {
-    name: "Me-Meet",
+    name: "PeerCallz",
     summary: "Peer to Peer video conferencing application.",
     desc1: "WebRTC is used for peer to peer connection.",
     desc2: "WebSockets are used at the server side.",
@@ -39,24 +37,25 @@ const projectList = [
       "tailwindCSS"
     ],
     sourceCodeUrl: "https://github.com/macci001/me-meet",
-    org: "me-meet.in"
+    projectUrl: "https://www.peercallz.com/",
+    org: "peercallz.com"
   },
-  {
-    name: "My-Tube",
-    summary: "Youtube Clone.",
-    desc1: "Frontend only React based web application.",
-    desc2: "Redux is used for store management",
-    desc3: "Google Youtube API is used for serving the results.",
-    technologies: [
-      "reactJS",
-      "authJS",
-      "redux",
-      "nextUI",
-      "tailwindCSS"
-    ],
-    sourceCodeUrl: "https://github.com/macci001/ytclone",
-    projectUrl: "https://yt-clone-gqih.onrender.com",
-    org: "my-tube.in"
-  }
+  // {
+  //   name: "My-Tube",
+  //   summary: "Youtube Clone.",
+  //   desc1: "Frontend only React based web application.",
+  //   desc2: "Redux is used for store management",
+  //   desc3: "Google Youtube API is used for serving the results.",
+  //   technologies: [
+  //     "reactJS",
+  //     "authJS",
+  //     "redux",
+  //     "nextUI",
+  //     "tailwindCSS"
+  //   ],
+  //   sourceCodeUrl: "https://github.com/macci001/ytclone",
+  //   projectUrl: "https://yt-clone-gqih.onrender.com",
+  //   org: "my-tube.in"
+  // }
 ];
 export default projectList;
