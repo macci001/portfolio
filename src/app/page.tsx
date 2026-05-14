d1869f0b313429cabe8d0150e90db6b55a134e29
Maharshi Alpesh <maharshialpesh@gmail.com>
2026-05-14 19:35:14 +0530

    chore: converting the portfolio for marketing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,14 @@ import { Projects } from "./_components/projects";
 import { Skills } from "./_components/skills";
 import { Socials } from "./_components/socials";
 import { Achievements } from "./_components/achievements";
+import Portfolio from "./_components/temp";
 
 export default function Page() {
     return (
     <>
     <ThemeProvider attribute="class" defaultTheme="dark">
       <NextUIProvider>
-        <div className="max-w-2xl mx-auto overflow-hidden">
+        {/* <div className="max-w-2xl mx-auto overflow-hidden">
           <Navbar />
           <div className="p-4">
             <Home />
@@ -21,7 +22,8 @@ export default function Page() {
             <Skills />
             <Socials className="justify-center" />
           </div>
-        </div>
+        </div> */}
+      <Portfolio />
       </NextUIProvider>
     </ThemeProvider>
     </>
